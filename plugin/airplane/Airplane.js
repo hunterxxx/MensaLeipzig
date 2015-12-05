e0e17d0c77ea5da95f0f4c0a43fe4a3952a973b9
Maximilian Springer <maximilian.springer@web.de>
2015-12-05 17:56:42 +0100

    Presentation code

--- plugin/airplane/Airplane.js
+++ plugin/airplane/Airplane.js
@@ -2,16 +2,17 @@ angular.module('cel.ng').directive('pluginAirplane', function() {
 	return {
 		link: function(scope, elem) {
 			// Unwrap jquery/jqLite wrapped element.
-			var rootElem = d3.select(elem.get(0)).append('div')
-				.style('position', 'absolute')
-				.style('width', '100%')
-				.style('height', '100%')
-				.append('img')
-				.attr('src', 'https://upload.wikimedia.org/wikipedia/commons/9/91/Winkel_triple_projection_SW.jpg')
+			var rootElem = d3.select(elem.get(0)).append('svg')
+				.attr('id', 'customsvg')
 				.style('width', '326px')
 				.style('height', '200px');
+
+   			d3.selectAll('#customsvg').append("svg:image")
+	        		.attr("xlink:href", "https://upload.wikimedia.org/wikipedia/commons/9/91/Winkel_triple_projection_SW.jpg")
+	        		.attr("width", 326)
+	        		.attr("height", 200);
 			var projection = d3.geo.winkel3()
-				.scale(150)
+				.scale(326)
 				.translate([326 / 2, 200 / 2])
 	    			.precision(.1);
 			// Do stuff with root elem.
@@ -36,11 +37,20 @@ angular.module('cel.ng').directive('pluginAirplane', function() {
 						'"TargetAirports.csv"."Longitude"' + 
 					')', 
 				false, scope.component).then(function(result) {
-					for(i=0; i<result.data.length; i++){
-						for(j=0; j<result.data[i].length; j++){
-							console.log(result.data[i][j]);
-						}						
+					var airlineArray = [];
+					for(i=0; i<result.data.length; i++) {
+						airlineArray[i] = [];
+						airlineArray[i][0] = result.data[i][0];
+						airlineArray[i][1] = result.data[i][1];
+						var sourceXY = projection([result.data[i][2], result.data[i][3]]);
+						airlineArray[i][2] = sourceXY[0];
+						airlineArray[i][3] = sourceXY[1];
+						var destXY = projection([result.data[i][4], result.data[i][5]]);
+						airlineArray[i][4] = destXY[0];
+						airlineArray[i][5] = destXY[1];
+						d3.selectAll('#customsvg').append('svg:circle').attr('cx', sourceXY[0]).attr('cy', sourceXY[1]).attr('r', 1).attr('fill', 'red');
 					}
+					console.log(airlineArray);
 				});
 			}
 		}
